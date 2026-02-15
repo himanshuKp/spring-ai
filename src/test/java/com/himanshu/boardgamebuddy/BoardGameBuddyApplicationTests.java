8c2650a9045e48114ab230a96de848bc5cb98d71
Himanshu Kandpal <himanshukp324@gmail.com>
2026-02-15 15:13:05 +0530

    project setup

---
+++ src/test/java/com/himanshu/boardgamebuddy/BoardGameBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.boardgamebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
